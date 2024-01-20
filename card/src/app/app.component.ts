dbcd6f6a9a51f547bb326678a595a86feb053f53
serhasHacizade <eren.felek.4@gmail.com>
2024-01-20 16:30:10 +0300

    commit

--- card/src/app/app.component.ts
+++ card/src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from "./card/card.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, CardComponent]
 })
 export class AppComponent {
-  title = 'card';
+  posts = [
+    {
+      title: "Mountain bike",
+      imageUrl: "assets/biking.jpeg",
+      username: "cbdag",
+      content: "I drive very fast"
+    },
+    {
+      title: "Hiking",
+      imageUrl: "assets/tree.jpeg",
+      username: "cbdag",
+      content: "Let's take a tour around Macka Park"
+    },
+    {
+      title: "Climbing",
+      imageUrl: "assets/mountain.jpeg",
+      username: "cbdag",
+      content: "I climbed well today too"
+    },
+  ]
 }
---
+++ card/src/app/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+  @Input() title:string = "";
+  @Input() imageUrl:string = "";
+  @Input() username:string = ""; 
+  @Input() content:string = "";
+}
