f5c9cf760c49bcedeeacc2adfcc0f3b83224b2d8
serhasHacizade <eren.felek.4@gmail.com>
2024-02-06 11:44:48 +0300

    commit

--- creditcard/src/app/cardform/cardform.component.ts
+++ creditcard/src/app/cardform/cardform.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { DateFormControl } from '../date-form-control';
 
 @Component({
   selector: 'app-cardform',
@@ -14,7 +15,27 @@ export class CardformComponent {
       Validators.required,
       Validators.minLength(3)
     ]),
-    
+    cardNumber: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
+    expiration: new DateFormControl(null, [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+    ]),
+    securityCode: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
+    ]),
   });
 
+  onSubmit() {
+    console.log("Erne");
+  };
+
+  ResetForm() {
+    this.cardForm.reset();
+  }
 }
