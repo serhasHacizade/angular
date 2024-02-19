9e750d1ebba9b75a751f77e0a30b0b649d43d3eb
serhasHacizade <eren.felek.4@gmail.com>
2024-02-19 10:33:12 +0300

    commit

---
+++ mathPlay/src/app/equality/equality.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equality',
+  templateUrl: './equality.component.html',
+  styleUrl: './equality.component.css'
+})
+export class EqualityComponent {
+
+}
