3f72eb0f745272ec263f06740534bfd1b3cc35cd
serhasHacizade <eren.felek.4@gmail.com>
2024-02-20 14:56:41 +0300

    commit

--- mathPlay/src/app/equality/equality.component.ts
+++ mathPlay/src/app/equality/equality.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
+
+
 @Component({
   selector: 'app-equality',
   templateUrl: './equality.component.html',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class EqualityComponent {
 
+  mathForm = new FormGroup({
+    firstNumber : new FormControl(this.generateNumber()),
+    secondNumber : new FormControl(this.generateNumber()),
+    answer : new FormControl(""),
+  },
+  [
+    (form: AbstractControl) => {
+      console.log(form.value);
+      const {firstNumber, secondNumber, answer} = form.value;
+      if (firstNumber + secondNumber === parseInt(answer)) {
+        return null;
+      }
+      return {addition: true}
+    }
+  ]);
+
+
+  get firstNumber() {
+    return this.mathForm.value.firstNumber;
+  }
+  get secondNumber() {
+    return this.mathForm.value.secondNumber;
+  }
+
+
+  generateNumber() {
+    return Math.floor(Math.random() * 10);
+  }
 }
