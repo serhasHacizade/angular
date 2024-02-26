e17e0eac680c3cc231e315922bc3c0305f5195d7
serhasHacizade <eren.felek.4@gmail.com>
2024-02-26 11:38:33 +0300

    commit

---
+++ material/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon"
+import { MatButtonToggleModule } from "@angular/material/button-toggle"
+import { MatBadgeModule} from "@angular/material/badge"
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatBadgeModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
