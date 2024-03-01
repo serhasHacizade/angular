b5092787ee6eee46559af143c5ce5fd36aca4359
serhasHacizade <eren.felek.4@gmail.com>
2024-03-01 11:02:44 +0300

    commit

--- material/src/app/app.component.ts
+++ material/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +14,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'material';
   news = 58;
+  show = false;
+  opened=false;
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+  
+  getData() {
+    this.show = true;
+    setTimeout(() => {
+      this.show = false;
+    },3000);
+  }
+
+  openSide() {
+    this.opened = !this.opened;
+  }
 }
--- material/src/app/app.module.ts
+++ material/src/app/app.module.ts
@@ -9,7 +9,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon"
 import { MatButtonToggleModule } from "@angular/material/button-toggle"
-import { MatBadgeModule} from "@angular/material/badge"
+import { MatBadgeModule } from "@angular/material/badge"
+
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner"
+import { MatProgressBarModule } from "@angular/material/progress-bar"
+import { MatToolbarModule } from "@angular/material/toolbar"
+import { MatSidenavModule } from "@angular/material/sidenav"
+import { MatMenuModule } from "@angular/material/menu"
+import { MatListModule } from "@angular/material/list"
+import { MatDividerModule } from "@angular/material/divider"
+import { MatGridListModule } from "@angular/material/grid-list"
 
 @NgModule({
   declarations: [
@@ -22,7 +31,15 @@ import { MatBadgeModule} from "@angular/material/badge"
     MatButtonModule,
     MatIconModule,
     MatButtonToggleModule,
-    MatBadgeModule
+    MatBadgeModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatListModule,
+    MatDividerModule,
+    MatGridListModule
   ],
   providers: [
     provideAnimationsAsync()
