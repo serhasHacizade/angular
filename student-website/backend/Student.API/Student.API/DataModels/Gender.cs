01c2d0eb5dd8585aeadab4378773c40f45ca280f
serhasHacizade <eren.felek.4@gmail.com>
2024-08-01 15:07:42 +0300

    commit

--- student-website/backend/Student.API/Student.API/DataModels/Gender.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Student.API.DataModels
-{
-    public class Gender
-    {
-        public Guid Id{ get; set; }
-        public string Description{ get; set; }
-
-    }
-}
--- student-website/backend/Student.API/Student.API/Repositories/IStudentRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Student.API.Repositories
-{
-    public interface IStudentRepository
-    {
-        Task<List<Student.API.DataModels.Students>> GetAllStudentsAsync();
-        Task<Student.API.DataModels.Students> GetStudentAsync(Guid studentId);
-    }
-}
